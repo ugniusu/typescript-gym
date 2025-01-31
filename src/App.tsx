8e7848c5f354460020add7a0d443b59ed7c6857e
ugniususc <ugniususc@gmail.com>
2025-01-31 12:02:23 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+
+enum SelectedPage {
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "ourclasses",
+  ContactUs = "contactus",
+}
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+
+  return (
+    <div className="bg-gray-20 ">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
+}
+
+export default App;
