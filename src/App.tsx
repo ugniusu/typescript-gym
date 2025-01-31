bea9d9bc43283953aa4503ac6c7487e967f04adc
ugniususc <ugniususc@gmail.com>
2025-01-31 19:50:17 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/scenes/navbar";
-import { useState } from "react";
+import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
+import { useEffect, useState } from "react";
 
 enum SelectedPage {
   Home = "home",
@@ -13,9 +15,29 @@ function App() {
     SelectedPage.Home
   );
 
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="bg-gray-20 ">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
     </div>
   );
 }
